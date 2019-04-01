1bba446276bae3f0718714c00897a125bf4397b5
Lucas Blotta <l_blotta@hotmail.com>
2019-04-01 00:21:49 -0300

    Simpler bash cfg mgmt

--- playbooks/roles/bash/files/bashprof/20-xauto.sh
+++
@@ -1,5 +0,0 @@
-# Automatically start the X session when logged in
-if [[ ! $DISPLAY && $XDG_VTNR -eq 1 && -e "$HOME/.xinitrc" ]]; then
-    exec startx ~/.xinitrc i3
-fi
-# Nothing gets executed after exec
--- playbooks/roles/bash/files/rc/15-options.sh
+++
@@ -1,8 +0,0 @@
-
-# BASH OPTIONS
-
-# Alt+h to go to man page of command previous to cursor
-bind '"\eh": "\C-a\eb\ed\C-y\e#man \C-y\C-m\C-p\C-p\C-a\C-d\C-e"'
-
-# Check window size after every command
-shopt -s checkwinsize
