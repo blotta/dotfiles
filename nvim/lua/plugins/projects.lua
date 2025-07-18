dde599ee7df6d0079926d906dbee3fcc5e5b252f
Lucas Blotta <lucas@blotta.info>
2025-07-18 17:55:02 -0300

    project plugin formatting

--- nvim/lua/plugins/projects.lua
+++ nvim/lua/plugins/projects.lua
@@ -1,19 +1,18 @@
 return {
 	{
-	  "ahmedkhalf/project.nvim",
-	  config = function()
+		"ahmedkhalf/project.nvim",
+		config = function()
+			require("project_nvim").setup {
+				-- your configuration comes here
+				-- or leave it empty to use the default settings
+				-- refer to the configuration section below
+				detection_methods = { 'lsp', 'pattern' },
+				patterns = { '.git' },
+			}
 
-		require("project_nvim").setup {
-		  -- your configuration comes here
-		  -- or leave it empty to use the default settings
-		  -- refer to the configuration section below
-			detection_methods = { 'lsp', 'pattern' },
-			patterns = { '.git' },
-		}
+			require('telescope').load_extension('projects')
 
-		require('telescope').load_extension('projects')
-
-		vim.keymap.set('n', '<leader>fp', ':Telescope projects<CR>', { desc = 'Telescope projects' })
-	  end
+			vim.keymap.set('n', '<leader>fp', ':Telescope projects<CR>', { desc = 'Telescope projects' })
+		end
 	}
 }
