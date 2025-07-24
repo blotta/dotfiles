c1bb55a5dc63da47cb519e6c7392601f41039a20
Lucas Blotta <lucas@blotta.info>
2025-07-24 10:38:26 -0300

    nvim goto previou buffer binding

--- nvim/lua/config/keybinds.lua
+++ nvim/lua/config/keybinds.lua
@@ -23,3 +23,8 @@ vim.keymap.set('i', '<A-l>', '<Esc><C-w>l', opts)
 vim.keymap.set({ "n", "v" }, "<leader>y", '"+y', { desc = "Yank to system clipboard" })
 -- Paste from system clipboard
 vim.keymap.set("n", "<leader>p", '"+p', { desc = "Paste from system clipboard" })
+
+
+
+-- goto previous buffer
+vim.keymap.set('n', '<leader><leader>', ":b#<CR>", { desc = 'Goto previous buffer' })
